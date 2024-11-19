eeb3018f51b8c5ad2b7bcb7bcc3050288063d428
RanggaPrathama <ranggaprathama9@gmail.com>
2024-11-19 10:33:26 +0700

    [ADD] Menambahkan generate credentials random  user.csv

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import MinMaxScaler
+import string
+import random
 
 # === 1. Load datasets ===
 tourism = pd.read_csv("data/tourism_with_id.csv")
@@ -37,6 +39,37 @@ if ratings.isnull().sum().sum() > 0:
 if users.isnull().sum().sum() > 0:
     print("Nilai null ditemukan di dataset users.")
     users.dropna(inplace=True)  # Hapus baris dengan nilai null
+    
+ 
+# ==== Create Data Dummies User ==== 
+
+
+username_random = [
+    "rangga", "hamid","levina", "john", "betty", "jane", "mike", "susan", "alex", "david", "linda",
+    "robert", "karen", "paul", "laura", "kevin", "amy", "steve", "sarah"
+]
+
+
+def generate_username(row):
+    users = random.choice(username_random)
+    unique_number = random.randrange(1000, 9999)
+    return f"{users}{unique_number}"
+    
+def generate_email(username):
+    return f"{username}@gmail.com"
+
+def generate_password(username):
+    return f"{username}123"
+
+def generate_password(length=8):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for i in range(length))
+
+users["Username"] = users.apply(generate_username, axis=1)
+users["Email"] = users["Username"].apply(generate_email)
+users["Password"] = [generate_password() for _ in range(len(users))]
+
+
 
 # === 3. Preprocessing Tourism Data ===
 
@@ -59,6 +92,12 @@ description_matrix = tfidf.fit_transform(tourism['Description'].fillna(''))  # J
 # Simpan TF-IDF matrix ke dalam DataFrame dengan kolom numerik
 tfidf_df = pd.DataFrame(description_matrix.toarray(), columns=[f"TFIDF_{i}" for i in range(description_matrix.shape[1])])
 tourism = pd.concat([tourism.reset_index(drop=True), tfidf_df.reset_index(drop=True)], axis=1)
+
+
+print("========= AFTER PREPROCESSING =========")
+print(tourism.isnull().sum())
+print(ratings.isnull().sum())
+print(users.isnull().sum())
 
 
 # === 4. Ekspor hasil preprocessing ===
